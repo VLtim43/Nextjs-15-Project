29bef29d3a6b421e5a439a0c79ba43292f45bd35
VLtim43 <fernandoreal.valentim@gmail.com>
2025-01-30 17:30:55 -0300

    feat: components

--- src/app/(rs)/layout.tsx
+++ src/app/(rs)/layout.tsx
@@ -1,12 +1,13 @@
+import { Header } from "@/components/Header";
+
 export default async function RSlayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div>
-      {/* header */}
-      <div>{children}</div>
+    <div className="mx-au<to w-full max-w-7xl">
+      <Header /> <div className="px-4 py-2">{children}</div>
     </div>
   );
 }
---
+++ src/components/NavButton.tsx
@@ -0,0 +1,35 @@
+import { LucideIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import ShouldRender from "./ShouldRender";
+
+type Props = {
+  icon: LucideIcon;
+  label: string;
+  href: string;
+};
+
+const NavButton = ({ icon: Icon, label, href }: Props) => {
+  return (
+    <Button
+      variant={"ghost"}
+      size={"icon"}
+      aria-label={label}
+      title={label}
+      className="rounded-full"
+      asChild
+    >
+      <ShouldRender if={href}>
+        <Link href={href}>
+          <Icon />
+        </Link>
+      </ShouldRender>
+
+      <ShouldRender if={!href}>
+        <Icon />
+      </ShouldRender>
+    </Button>
+  );
+};
+
+export default NavButton;
